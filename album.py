aa1783e08483c1be3f75c9cebd0c0babb312848e
Alexsander Melo <alexsandergmelo337@gmail.com>
2023-12-04 09:28:33 -0300

    ainda falta

--- album.py
+++ album.py
@@ -61,7 +61,6 @@ Builder.load_string("""
                     id: titulo
                     text: 'Quiz'
                     halign: 'center'
-                    font_name: 'robotoblack.ttf'
                     font_size: '40sp'
                     size_hint: 1.0, 1.0
                 AnchorLayout:
